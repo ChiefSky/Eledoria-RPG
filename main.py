9c7151cd0e8a1abd5a9342f1a0a013846ca67d8e
ChiefSky <sergeiglad29@gmail.com>
2025-03-08 15:41:02 +0700

    basic shop functionality

--- main.py
+++ main.py
@@ -6,6 +6,65 @@ coins = 0
 
 def printParemeters():
     print("У тебя {0} здоровья, {1} атаки, {2} монет.".format(hp, damage, coins))
+    
+def printHp():
+    print("У тебя", hp, "здоровья")
+    
+def printDamage():
+    print("У тебя", damage, "урона")
+    
+def printCoins():
+    print("У тебя", coins, "монет")
+    
+def meetShop():
+    global hp
+    global damage
+    global coins
+    
+    def buy(cost):
+        global coins
+        if coins >= cost:
+            coins -= cost
+            printCoins()
+            return True
+        print("Мало денег!")
+        return False
+    
+    weaponLvl = rand.randint(1, 5)
+    weaponDamage = rand.randint(1,5) * weaponLvl
+    weapons = ["Палка-копалка", "Молот Громовержца", "Лук Эльфа", "Посох мага", "Алмазный меч"]
+    weaponRarities = ["Испорченный", "Обычный", "Редкий", "Эпический", "Легендарный"]
+    weaponRarity = weaponRarities[weaponLvl - 1]
+    weaponCost = rand.randint(3, 10) * weaponLvl
+    weapon = rand.choice(weapons)
+    
+    oneHpCost = 5
+    threeHpCost = 12
+    
+    print("Вам встретился магазин")
+    printParemeters()
+    
+    while input("Что будешь делать? (зайти/уйти): ").lower() == "зайти":
+        print ("1) Одна единица здоровья - ", oneHpCost, "монет")
+        print ("2) Три единицы здоровья - ", threeHpCost, "монет")
+        print ("3) {0}, {1}, {2} монет".format(weaponRarity, weapon, weaponCost))
+    
+        choice = input("Что хочешь приобрести?")
+        if choice == "1":
+            if buy(oneHpCost):
+                hp+=1
+                printHp()
+        elif choice == "2":
+            if buy(threeHpCost):
+                hp+=3
+                printHp()
+        elif choice == "3":
+            if buy(weaponCost):
+                damage = weaponDamage
+                printDamage()
+        else:
+            print("Я такое не продаю")
+                
 
 def initGame(initHp, initDamage, initCoins):
     global hp
@@ -22,14 +81,14 @@ def initGame(initHp, initDamage, initCoins):
 def gameLoop():
     situation = rand.randint(0, 3)
     if situation == 0:
-        input("Вы встретили магазин")
+        meetShop()
     elif situation == 1:
         input("Вы встретили монстра")
     else:
         input("Блуждаем...")
         
         
-initGame(3, 1, 5)
+initGame(3, 1, 999)
 
 while True:
     gameLoop()
